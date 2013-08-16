c4fe426e78cb2e681edc4d7040dcd9a904472da3
Bruce Morrison <bruce@stuffandcontent.com>
2013-08-16 17:21:16 +1000

    Allow jetty to be removed.

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,22 +1,53 @@
 #
 class jetty::service {
   include jetty
-
   if $jetty::ensure == 'present' {
-    $service_ensure = 'running'
-    $service_enable = true
+
+    case $jetty::status {
+      # make sure service is currently running, start it on boot
+      'enabled': {
+        $service_ensure = 'running'
+        $service_enable = true
+      }
+      # make sure service is currently stopped, do not start it on boot
+      'disabled': {
+        $service_ensure = 'stopped'
+        $service_enable = false
+      }
+      # make sure service is currently running, do not start it on boot
+      'running': {
+        $service_ensure = 'running'
+        $service_enable = false
+      }
+      # do not start service on boot, do not care whether currently running or not
+      'unmanaged': {
+        $service_ensure = undef
+        $service_enable = false
+      }
+      # unknown status
+      # note: don't forget to update the parameter check in init.pp if you
+      #     add a new or change an existing status.
+      default: {
+        fail("\"${jetty::status}\" is an unknown service status value")
+      }
+    }
+
+  # set params: removal
   } else {
+
+    # make sure the service is stopped and disabled (the removal itself will be
+    # done by package.pp)
     $service_ensure = 'stopped'
     $service_enable = false
+
   }
 
   file { '/etc/default/jetty':
-    ensure  => 'present',
+    ensure  => 'file',
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     content => template("${module_name}/etc/default/jetty.erb"),
-    require => Class['jetty::package'],
     notify  => Service['jetty'],
   }
 
